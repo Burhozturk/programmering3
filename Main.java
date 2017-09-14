6cb31873d9cc404cf6e68632f252775c04401a03
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2017-09-14 14:16:25 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        //Udskriver hello world
+        System.out.println("Hello World!");
+        //Variabel af integer
+        int etTal=4;
+        //Variabel af tekst
+        String enTekst= " Pølse ";
+        //For løkke, som kører antal gange
+        for(int i=0; i < etTal; i++)
+        {
+            //Udskriver
+            System.out.print(i+1);
+            System.out.println(enTekst);
+        }
+    }
+}
