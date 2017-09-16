e2a01b1eb43f5ce586d9c35d3699178c22c232a1
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2017-09-16 12:18:32 +0200

    Add files via upload

---
+++ 1.2.java
@@ -0,0 +1,19 @@
+public class Main{
+
+
+
+    public static void main(String[] args) {
+
+
+	//Udskriver disse tre strings
+        System.out.println("Welcome to Java");
+
+        System.out.println("Learning Java Now");
+
+        System.out.println("Programming is fun");
+
+
+
+    }
+
+}
